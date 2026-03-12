86e2a736c2872cd1a09b914de7598397d9156796
Dimmis5 <zhangdimeo@gmail.com>
2026-03-12 16:15:56 +0100

    modification css

--- next.config.js
+++ next.config.js
@@ -1,36 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    font-src 'self' https://fonts.gstatic.com;
-    img-src 'self' blob: data: https://investdays.garageisep.com;
-    connect-src 'self' https://portail-ovh.isep.fr ws: wss:;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`.replace(/\s{2,}/g, ' ').trim();
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader,
-          },
-        ],
-      },
-    ];
+  turbopack: {},
+  env: {
   },
 };
-
 module.exports = nextConfig;
